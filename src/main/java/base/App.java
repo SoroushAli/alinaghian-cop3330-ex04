2bb6d69bef12d28fd234422b426c0c97f21ec382
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 02:30:41 -0400

    Solution for Exercise 4

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,6 +3,9 @@
  *  Copyright 2021 Soroush Alinaghian
  */
 package base;
+
+import java.util.Scanner;
+
 /*
 Example Output
 
@@ -23,7 +26,33 @@ Add more inputs to the program to expand the story.
 Implement a branching story, where the answers to questions determine how the story is constructed.
  */
 public class App {
-    public static void main(String[] args) {
+    static Scanner in = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        String noun = getNoun();
+        String verb = getVerb();
+        String adj = getAdjective();
+        String adverb = getAdverb();
+        printOutput(noun, verb, adj, adverb);
+    }
+
+    private static void printOutput(String noun, String verb, String adj, String adverb) {
+        System.out.println(String.format("Enter a noun: %s\nEnter a verb: %s\nEnter an adjective: %s\nEnter an adverb: %s\nDo you %s your %s %s %s? That's hilarious!", noun, verb, adj, adverb, verb, adj, noun, adverb));
+    }
+
+    private static String getAdverb() {
+        return in.nextLine();
+    }
+
+    private static String getAdjective() {
+        return in.nextLine();
+    }
+
+    private static String getVerb() {
+        return in.nextLine();
+    }
+
+    private static String getNoun() {
+        return in.nextLine();
     }
 }
