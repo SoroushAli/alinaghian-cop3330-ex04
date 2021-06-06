6cbf7e5996813f831db9e419c469d26525c12930
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 02:07:22 -0400

    adding Instructions for Exercise 4

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,7 +1,29 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Soroush Alinaghian
+ */
 package base;
+/*
+Example Output
 
+Enter a noun: dog
+Enter a verb: walk
+Enter an adjective: blue
+Enter an adverb: quickly
+Do you walk your blue dog quickly? That's hilarious!
+
+Constraints
+
+Use a single output statement for this program.
+If your language supports string interpolation or string substitution, use it to build up the output.
+
+Challenges
+
+Add more inputs to the program to expand the story.
+Implement a branching story, where the answers to questions determine how the story is constructed.
+ */
 public class App {
     public static void main(String[] args) {
-        
+
     }
 }
